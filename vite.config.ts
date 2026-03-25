102610ad3185809d84e1baf3dbdb81fc3e5fd8dc
dushyantsingh08 <dushyantsingh2775@gmail.com>
2026-03-25 19:31:52 +0530

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 2500, // Supress the 500kb warnings for PDF processing tools
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('client/node_modules/pdf-lib') || id.includes('pdf-lib') || id.includes('pdfjs-dist')) return 'pdf-engine';
+          if (id.includes('docx')) return 'docx-engine';
+          if (id.includes('react') || id.includes('react-dom')) return 'react-vendor';
+        }
+      }
+    }
+  }
 })
